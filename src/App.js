b1dbf3d3a34857213dc52a85a46588f68408f29a
Abstract02 <adarsh526222@gmail.com>
2022-07-27 18:05:33 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import User from './Components/User'
-import {BiSearchAlt2} from 'react-icons/bi'
+import { BiSearchAlt2 } from 'react-icons/bi'
 import Menus from './Components/Menus';
 import Logo from './Components/images/Logo.jpg'
 import Activity from './Components/Activity';
@@ -10,31 +10,31 @@ import MainDashBoard from './Components/MainDashBoard';
 function App() {
   return (
     <>
-    <div className='dashboard'>
-      <div className='header'>
-        <div className='logo'>
-          <img src={Logo} alt='logo' />
+      <div className='dashboard'>
+        <div className='header'>
+          <div className='logo'>
+            <img src={Logo} alt='logo' />
+          </div>
+          <div className='search_bar'>
+            <BiSearchAlt2 className='search_icon' />
+            <input placeholder='searh for projects' />
+          </div>
+          <div className='user'>
+            <User />
+          </div>
         </div>
-        <div className='search_bar'>
-          <BiSearchAlt2 className='search_icon' />
-          <input placeholder='searh for projects' />
-        </div>
-        <div className='user'>
-          <User />
+        <div className='main'>
+          <div className='menu_board'>
+            <Menus />
+          </div>
+          <div className='dashboard_main'>
+            <MainDashBoard />
+          </div>
+          <div className='activity_section'>
+            <Activity />
+          </div>
         </div>
       </div>
-      <div className='main'>
-        <div className='menu_board'>
-          <Menus />
-        </div>
-        <div className='dashboard_main'>
-          <MainDashBoard />
-        </div>
-        <div className='activity_section'>
-          <Activity />
-        </div>
-      </div>
-    </div>
     </>
   );
 }
